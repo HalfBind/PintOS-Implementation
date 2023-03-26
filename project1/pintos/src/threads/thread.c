a902897a1e2c6b3e3c2dd1a8e7209f8020d64f3b
sis3141 <sis3141@unist.ac.kr>
2023-03-26 14:18:41 +0900

    hotfix: remove duplicate priority scheduling in getting next thread remove unused folder

--- project1/pintos/src/threads/thread.c
+++ project1/pintos/src/threads/thread.c
@@ -321,7 +321,7 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered (&ready_list, &cur->elem, less_priority, NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -502,7 +502,6 @@ next_thread_to_run (void)
     return idle_thread;
   else
     {
-      list_sort(&ready_list, less_priority, NULL);
       return list_entry (list_pop_front (&ready_list), struct thread, elem);
     }
 }
