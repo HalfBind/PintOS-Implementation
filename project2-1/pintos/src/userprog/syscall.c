2fe863ae7ac8016bb55c8d9676a144b9ab63fcc5
delphox60 <kb28@unist.ac.kr>
2023-04-30 00:17:16 +0900

    feat: export checking address function

--- project2-1/pintos/src/userprog/syscall.c
+++ project2-1/pintos/src/userprog/syscall.c
@@ -18,3 +18,12 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
   thread_exit ();
 }
+
+void validate_user_vaddr (const void *vaddr) {
+  if (!is_user_vaddr(vaddr))
+    exit(-1); // TODO implement
+  if (vaddr == NULL)
+    exit(-1); // TODO implement
+  if (pagedir_get_page(thread_current()->pagedir, vaddr) == NULL)
+    exit(-1); // TODO implement
+}
