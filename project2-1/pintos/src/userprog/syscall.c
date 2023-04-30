1c0f00892cc15c16adc93e396cf4ab7828615744
delphox60 <kb28@unist.ac.kr>
2023-04-30 13:46:44 +0900

    feat: implement exit

--- project2-1/pintos/src/userprog/syscall.c
+++ project2-1/pintos/src/userprog/syscall.c
@@ -34,17 +34,20 @@ syscall_handler (struct intr_frame *f UNUSED)
 
 void validate_user_vaddr (const void *vaddr) {
   if (!is_user_vaddr(vaddr))
-    // exit(-1); // TODO implement
-    printf("exit");
+    exit(-1);
   if (vaddr == NULL)
-    // exit(-1); // TODO implement
-    printf("exit");
+    exit(-1);
   if (pagedir_get_page(thread_current()->pagedir, vaddr) == NULL)
-    // exit(-1); // TODO implement
-    printf("exit");
+    exit(-1);
 }
 
 uint32_t get_argument(int32_t *esp, int offset) {
   validate_user_vaddr(esp + offset);
   return *(esp + offset);
+}
+
+void exit (int status)
+{
+  printf("exit program. status: %d", status);
+  thread_exit();
 }
